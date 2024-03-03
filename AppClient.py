82efff1d66df31a6d097eddb3b76ce898a9531ca
PrincessDreamland <285835609@qq.com>
2024-03-04 01:58:17 +0800

    3.4-1

--- AppClient.py
+++ AppClient.py
@@ -4,13 +4,16 @@ os.chdir(os.path.dirname(sys.executable) if getattr(sys, 'frozen', False) else o
 from Hotaru.Client.LogClientHotaru import logClientMgr
 from Hotaru.Client.StateHotaru import stateMgr
 from States.InitState import InitState
+from Hotaru.Client.OcrClientHotaru import ocrClientMgr
 
 class AppClient:
     def Main():
         stateMgr.Transition(InitState())
         input("test...")
 
-
+def ExitHandler():
+    # 退出 OCR
+    ocrClientMgr.mOcr.ExitOcr()
 
 if __name__ == "__main__":
     if not pyuac.isUserAdmin():
@@ -18,13 +21,13 @@ if __name__ == "__main__":
             pyuac.runAsAdmin(wait=False)
             sys.exit(0)
         except Exception:
-            # logger.error("管理员权限获取失败")
+            logClientMgr.Error("管理员权限获取失败")
             input("按回车键关闭窗口. . .")
             sys.exit(0)
     else:
         try:
+            atexit.register(ExitHandler)
             AppClient.Main()
-            # atexit.register(exit_handler)
             # main(sys.argv[1]) if len(sys.argv) > 1 else main()
         except KeyboardInterrupt:
             logClientMgr.Error("发生错误: 手动强制停止")
--- Modules/Client/DetectScreenSubModule.py
+++ Modules/Client/DetectScreenSubModule.py
@@ -1,5 +1,24 @@
+import numpy as np,time,math,cv2
 
 class DetectScreenSubModule:
+
+    mInstance = None
+
+    def __new__(cls, window_title):
+        if cls.mInstance is None:
+            cls.mInstance = super().__new__(cls)
+            cls.window_title = window_title
+            cls.screenshot = None
+            cls.mInstance.InitDetectScreen()
+
+        return cls.mInstance
     
-    def __init__(self):
-        pass
+    # 兼容旧代码
+    def InitDetectScreen(self):
+        self.mouse_click = Input.mouse_click
+        self.mouse_down = Input.mouse_down
+        self.mouse_up = Input.mouse_up
+        self.mouse_move = Input.mouse_move
+        self.mouse_scroll = Input.mouse_scroll
+        self.press_key = Input.press_key
+        self.press_mouse = Input.press_mouse
