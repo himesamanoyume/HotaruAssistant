4b7cf82ae3c0c6f60057464458a3d40a43a0d91a
PrincessDreamland <285835609@qq.com>
2024-03-09 09:50:08 +0800

    Update DevScreenSubModule.py

--- Modules/Client/DevScreenSubModule.py
+++ Modules/Client/DevScreenSubModule.py
@@ -1,7 +1,9 @@
-import pyautogui,tkinter,os
+import pyautogui,tkinter,os,time
 from Hotaru.Client.LogClientHotaru import logMgr,log
+from Hotaru.Client.ConfigClientHotaru import configMgr
 from tkinter import *
 from Modules.Utils.GameWindow import GameWindow
+from PIL import Image, ImageTk
 
 
 class DevScreenSubModule:
@@ -9,7 +11,7 @@ class DevScreenSubModule:
     def IsApplicationFullscreen(self, window):
         screenWidth, screenHeight = pyautogui.size()
         return (window.width, window.height) == (screenWidth, screenHeight)
-
+    
     def GetWindowRegion(self, window):
         # 边框
         otherBorder = (window.width - 1920) // 2
@@ -18,32 +20,52 @@ class DevScreenSubModule:
         if self.IsApplicationFullscreen(window):
             return (window.left, window.top, window.width, window.height)
         else:
+            return (window.left + otherBorder, window.top + upBorder, window.width -
+                    otherBorder - otherBorder, window.height - upBorder - otherBorder)
+
+    def GetWindowDevRegion(self, window):
+        # 边框
+        otherBorder = (window.width - 1920) // 2
+        upBorder = window.height - 1080 - otherBorder
+
+        if self.IsApplicationFullscreen(window):
+            return (window.left, window.top, window.width, window.height)
+        else:
             return (window.left, window.top - upBorder, window.width -
-                    otherBorder - otherBorder, window.height)
+                    otherBorder - otherBorder, window.height + upBorder + otherBorder)
     
     def GetHonkaiWindowsInfo(self, window, crop=(0, 0, 0, 0)):
         if crop == (0, 0, 0, 0):
-            screenshotPos = self.GetWindowRegion(window)
+            screenshotPos = self.GetWindowDevRegion(window)
             return screenshotPos
         else:
-            left, top, width, height = self.GetWindowRegion(window)
+            left, top, width, height = self.GetWindowDevRegion(window)
             screenshotPos = int(left + width * crop[0]), int(top + height * crop[1]), int(width * crop[2]), int(height * crop[3])
             return screenshotPos
         
-
-    def TakeScreenshot(self, title, crop=(0, 0, 0, 0)):
+    def TakeScreenshot(self, crop=(0, 0, 0, 0)):
+        # self.canvas.create_rectangle(50, 50, self.canvas.winfo_width() / 2, self.canvas.winfo_height() / 2, outline="red", width=3)
+        # self.isScreenshot = True
+        # time.sleep(1)
+        # --------
+        title = configMgr.mConfig[configMgr.mKey.GAME_TITLE_NAME]
         window = GameWindow.GetWindow(title)
         if window:
             if crop == (0, 0, 0, 0):
-                screenshot_pos = self.GetWindowRegion(window)
+                screenshotPos = self.GetWindowRegion(window)
             else:
                 left, top, width, height = self.GetWindowRegion(window)
-                screenshot_pos = int(left + width * crop[0]), int(top + height * crop[1]), int(width * crop[2]), int(height * crop[3])
+                screenshotPos = int(left + width * crop[0]), int(top + height * crop[1]), int(width * crop[2]), int(height * crop[3])
 
-            screenshot = pyautogui.screenshot(region=screenshot_pos)
-            return screenshot, screenshot_pos
-
-        return False
+            GameWindow.SwitchToWindow(title, maxRetries=4)
+            self.screenshot = pyautogui.screenshot(region=screenshotPos)
+            self.isScreenshot = True
+        # -----------------
+            # return screenshot, screenshot_pos
+        # return False
+            
+    def CloseScreenshot(self):
+        self.isScreenshot = False
         
     def ShowInputArea(self, inputArea):
         pass
@@ -102,12 +124,12 @@ class DevScreenSubModule:
     #     else:
     #         tkinter.messagebox.showinfo("结果", "还没有选择区域呢")
 
-    # def SaveScreenshot(self):
-    #     if not os.path.exists("screenshots"):
-    #         os.makedirs("screenshots")
-    #     screenshot_path = os.path.abspath(r"screenshots\screenshot.png")
-    #     self.screenshot.save(screenshot_path)
-    #     os.startfile(os.path.dirname(screenshot_path))
+    def SaveScreenshot(self):
+        if not os.path.exists("screenshots"):
+            os.makedirs("screenshots")
+        screenshotPath = os.path.abspath(r"screenshots\screenshot.png")
+        self.screenshot.save(screenshotPath)
+        os.startfile(os.path.dirname(screenshotPath))
 
     # def SaveSelection(self):
     #     if self.selection_rect:
@@ -138,8 +160,13 @@ class DevScreenSubModule:
                     self.tk.title('DevScreen')
 
                     self.tk.configure(width=evt.width,height=evt.height)
-                    self.canvas.create_rectangle(0, 0, self.canvas.winfo_width(), self.canvas.winfo_height(), fill=TRANSCOLOUR, outline=TRANSCOLOUR)
-                    self.canvas.create_rectangle(0, 0, self.canvas.winfo_width() / 2, self.canvas.winfo_height() / 2, outline="red", width=3)
+                  
+                    if self.isScreenshot:
+                        print("有截图")
+                        photo = ImageTk.PhotoImage(self.screenshot)
+                        self.canvas.create_image(0, 0, anchor=tkinter.NW, image=photo)
+                    else:
+                        self.canvas.create_rectangle(0, 0, self.canvas.winfo_width(), self.canvas.winfo_height(), fill=TRANSCOLOUR, outline=TRANSCOLOUR)
 
                 except Exception:
                     log.error(logMgr.Error("窗口将自动关闭"))
@@ -147,6 +174,7 @@ class DevScreenSubModule:
 
             self.tk = tkinter.Tk()
             self.tk.attributes("-topmost", 1)
+            self.isScreenshot = False
 
             TRANSCOLOUR = 'gray'
             self.tk.wm_attributes('-transparentcolor', TRANSCOLOUR)
@@ -157,16 +185,14 @@ class DevScreenSubModule:
             self.canvas.bind("<B1-Motion>", self.OnMouseDrag)
             self.canvas.bind("<ButtonRelease-1>", self.OnButtonRelease)
 
-            self.TakeScreenshotButton = tkinter.Button(self.tk, text="截图", command=self.TakeScreenshot)
-            self.TakeScreenshotButton.pack(side=tkinter.LEFT, padx=5, pady=5)
-            self.ShowResultButton = tkinter.Button(self.tk, text="显示坐标", command=self.ShowResult)
-            self.ShowResultButton.pack(side=tkinter.LEFT, padx=5, pady=5)
-
             self.canvas.pack(fill=BOTH,expand=Y)
+
+            self.takeScreenButton = tkinter.Button(self.tk, text="截图", command=self.TakeScreenshot)
+            self.takeScreenButton.pack(side=tkinter.LEFT, padx=5, pady=5)
+            self.save_screenshot_button = tkinter.Button(self.tk, text="保存完整截图", command=self.SaveScreenshot)
+            self.save_screenshot_button.pack(side=tkinter.LEFT, padx=5, pady=5)
             
             self.tk.bind('<Configure>', OnSize)
-            
-
             
 
             self.tk.mainloop()
