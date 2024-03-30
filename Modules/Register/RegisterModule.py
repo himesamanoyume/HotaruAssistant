17057f9b8d7d8681501724c62ddf8ebfbc7b003b
PrincessDreamland <285835609@qq.com>
2024-03-31 01:06:17 +0800

    3.31-1

--- Modules/Register/RegisterModule.py
+++
@@ -1,7 +0,0 @@
-class RegisterModule:
-    mInstance = None
-
-    def __new__(cls):
-        if cls.mInstance is None:
-            cls.mInstance = super().__new__(cls)
-        return cls.mInstance
